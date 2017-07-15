59414c751012e1c506b41e94a285f987754e423d
Brian Puschell <bpuschel@e1z2r5p20.42.us.org>
2017-07-14 21:00:39 -0700

    Initial first draft of stack functions

--- includes/stack.h
+++ includes/stack.h
@@ -6,17 +6,17 @@
 /*   By: bpuschel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 18:02:13 by bpuschel          #+#    #+#             */
-/*   Updated: 2017/07/14 18:34:04 by bpuschel         ###   ########.fr       */
+/*   Updated: 2017/07/14 20:14:33 by bpuschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_H
 # define STACK_H
-# include "../libft/ft_printf/ft_printf.h"
+# include "../libft/ft_printf.h"
 
 void	push(t_list **stack, void* elem, size_t size);
-void	*pop(void);
-void	shift(t_list **stack);
+t_list	*pop(t_list **stack);
+void	bubble_up(t_list **stack);
 void	swap(t_list **a, t_list **b);
 void	rotate(t_list **stack);
 
--- srcs/stack.c
+++ srcs/stack.c
@@ -6,10 +6,59 @@
 /*   By: bpuschel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 18:46:06 by bpuschel          #+#    #+#             */
-/*   Updated: 2017/07/14 18:46:40 by bpuschel         ###   ########.fr       */
+/*   Updated: 2017/07/14 20:55:35 by bpuschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/stack.h"
 
+void	push(t_list **stack, void* elem, size_t size)
+{
+	t_list *to_add;
+	
+	to_add = ft_lstnew(elem, size);
+	ft_lstadd(stack, to_add);
+	free(to_add);
+}
 
+t_list	*pop(t_list **stack)
+{
+	t_list *out;
+	
+	out = *stack;
+	*stack = out->next;
+	out->next = NULL;
+	return (out);
+}
+
+void	bubble_up(t_list **stack)
+{
+	t_list *temp;
+
+	temp = *stack;
+	temp->next = (*stack)->next->next;
+	*stack = (*stack)->next;
+	(*stack)->next = temp;
+	free (temp);
+}
+
+void	swap(t_list **a, t_list **b)
+{
+	t_list *temp;
+
+	temp = pop(a);
+	push(b, temp->content, temp->content_size);
+	free(temp);
+}
+
+void	rotate(t_list **stack)
+{
+	t_list *end;
+	t_list *curr;
+
+	end = pop(stack);
+	curr = *stack;
+	while (curr->next != NULL)
+		curr = curr->next;
+	curr->next = end;
+}
