6468752ee7cbb2e1bd987fd728453cada732bdad
Brian Puschell <bpuschel@e1z2r5p12.42.us.org>
2017-07-22 18:50:42 -0700

    Added descriptions to bubble_up and swap functions. Added parse_cmd function.

---
+++ srcs/parse_cmd.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_cmd.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpuschel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/22 18:30:17 by bpuschel          #+#    #+#             */
+/*   Updated: 2017/07/22 18:49:48 by bpuschel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	parse_cmd(char *cmd, t_list **a, t_list **b)
+{
+	if (ft_strequ(cmd, "sa") || ft_strequ(cmd, "ss"))
+		bubble_up(a);
+	if (ft_strequ(cmd, "sb") || ft_strequ(cmd, "ss"))
+		bubble_up(b);
+	if (ft_strequ(cmd, "pa"))
+		swap(b, a);
+	if (ft_strequ(cmd, "pb"))
+		swap(a, b);
+	if (ft_strequ(cmd, "ra") || ft_strequ(cmd, "rr"))
+		rotate(a);
+	if (ft_strequ(cmd, "rb") || ft_strequ(cmd, "rr"))
+		rotate(b);
+	if (ft_strequ(cmd, "rra") || ft_strequ(cmd, "rrr"))
+		rrotate(a);
+	if (ft_strequ(cmd, "rrb") || ft_strequ(cmd, "rrr"))
+		rrotate(b);
+}
--- srcs/stack.c
+++ srcs/stack.c
@@ -6,7 +6,7 @@
 /*   By: bpuschel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 18:46:06 by bpuschel          #+#    #+#             */
-/*   Updated: 2017/07/22 17:48:52 by bpuschel         ###   ########.fr       */
+/*   Updated: 2017/07/22 18:05:13 by bpuschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,24 +33,49 @@ t_list	*pop(t_list **stack)
 	return (out);
 }
 
+/*
+** void bubble_up(t_list **stack)
+**
+** bubble_up swaps the first two elements at the top of the stack. Does nothing
+** if stack contains one or no elements.
+*/
+
 void	bubble_up(t_list **stack)
 {
 	t_list *temp;
+	int size;
 
 	temp = *stack;
-	temp->next = (*stack)->next->next;
-	*stack = (*stack)->next;
-	(*stack)->next = temp;
-	free(temp);
+	size = 0;
+	while (temp != NULL || size++)
+		temp = temp->next;
+	if (size > 1)
+	{
+		temp = *stack;
+		temp->next = (*stack)->next->next;
+		*stack = (*stack)->next;
+		(*stack)->next = temp;
+		free(temp);
+	}
 }
+
+/*
+** void swap(t_list **a, t_list **b)
+**
+** swap takes the first element at the top of a and puts it on the top of b.
+** Does nothing if a is empty.
+*/
 
 void	swap(t_list **a, t_list **b)
 {
 	t_list *temp;
 
-	temp = pop(a);
-	push(b, temp->content, temp->content_size);
-	free(temp);
+	if (*a != NULL)
+	{
+		temp = pop(a);
+		push(b, temp->content, temp->content_size);
+		free(temp);
+	}
 }
 
 void	rotate(t_list **stack)
